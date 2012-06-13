353a54ae6377565f06cf2fc1ff9617722bf2e59d
lmaxim <lmaxim@lmaxim.com>
2012-06-13 14:30:32 +0400

    staging commit

---
+++ lib/config/sfAzureConfig.class.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lmaxim
+ * Date: 6/9/12
+ * Time: 5:50 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+class sfAzureConfig
+{
+
+
+}
---
+++ lib/helper/AzureStorageUrlHelper.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: lmaxim
+ * Date: 6/9/12
+ * Time: 4:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+ 
